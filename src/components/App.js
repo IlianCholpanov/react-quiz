a7c6b796a1eb9663e753b3517562ba35809405f2
Ilian Cholpanov <iliancholpanov@gmail.com>
2024-02-27 17:12:41 +0200

    .

--- src/components/App.js
+++ src/components/App.js
@@ -60,6 +60,13 @@ function reducer(state, action) {
         highscore:
           state.points > state.highscore ? state.points : state.highscore,
       };
+    case "reset" /* might not need */:
+      return {
+        ...initialState,
+        questions: state.questions,
+        status: "ready",
+        /*index: 0, answer: null, points: 0*/
+      };
 
     default:
       throw new Error("Action unknown");
@@ -120,6 +127,7 @@ export default function App() {
             points={points}
             maxPossiblePoints={maxPossiblePoints}
             highscore={highscore}
+            dispatch={dispatch}
           />
         )}
       </Main>
--- src/components/FinishScreen.js
+++ src/components/FinishScreen.js
@@ -1,4 +1,4 @@
-function FinishScreen({ points, maxPossiblePoints, highscore }) {
+function FinishScreen({ points, maxPossiblePoints, highscore, dispatch }) {
   const percentage = (points / maxPossiblePoints) * 100;
 
   let emoji = "";
@@ -15,6 +15,13 @@ function FinishScreen({ points, maxPossiblePoints, highscore }) {
         {maxPossiblePoints} ({Math.ceil(percentage)}%)
       </p>
       <p className="highscore">(Highscore is {highscore} points)</p>
+
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "reset" })}
+      >
+        Restart quiz
+      </button>
     </>
   );
 }
